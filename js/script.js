ae22baef172b55b761aa7d475e4b26aaed35600c
Vitor <96145558+viteydev@users.noreply.github.com>
2022-04-02 20:35:47 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 import addNumb from "./modules/addNumb.js";
 import verifyInput from './modules/verifyInput.js'
 import counter from './modules/counter.js'
+import clock from './modules/clock.js'
 
 counter()
 addNumb()
 verifyInput()
+
+const Display = new clock({
+    hour:true,
+    minute:true,
+    second:true
+}).init()
 
